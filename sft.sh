47f3fd2f3d3a980f83485f2f2361fd0e51af346c
Yang Dixuan <e1553316@asp2a-login-nus02.head.cm.asp2a.nscc.sg>
2026-02-09 12:41:58 +0800

    Initial commit

---
+++ sft.sh
@@ -0,0 +1,26 @@
+export NCCL_IB_DISABLE=1        # 完全禁用 IB/RoCE
+# Office_Products, Industrial_and_Scientific
+for category in "Industrial_and_Scientific"; do
+    train_file=$(ls -f ./data/Amazon/train/${category}*11.csv)
+    eval_file=$(ls -f ./data/Amazon/valid/${category}*11.csv)
+    test_file=$(ls -f ./data/Amazon/test/${category}*11.csv)
+    info_file=$(ls -f ./data/Amazon/info/${category}*.txt)
+    echo ${train_file} ${eval_file} ${info_file} ${test_file}
+    
+    torchrun --nproc_per_node 2 \
+            sft.py \
+            --base_model ./models/Qwen2.5-1.5B \
+            --batch_size 64 \
+            --micro_batch_size 8 \
+            --train_file ${train_file} \
+            --eval_file ${eval_file} \
+            --output_dir output_dir/xxx \
+            --wandb_project wandb_proj \
+            --wandb_run_name wandb_name \
+            --category ${category} \
+            --train_from_scratch False \
+            --seed 42 \
+            --sid_index_path ./data/Amazon/index/Industrial_and_Scientific.index.json \
+            --item_meta_path ./data/Amazon/index//Industrial_and_Scientific.item.json \
+            --freeze_LLM False
+done
